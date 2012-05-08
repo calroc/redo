917a64cbc1d685a44ae67595b89eb45902734880
Simon Forman <forman.simon@gmail.com>
2012-05-07 23:11:47 -0700

    Explicit imports for cleaner code.
    Should maybe use os.path.split and .join for the path manipulations
    except that I think non-'/' systems aren't supported anyway...

--- vars_init.py
+++ vars_init.py
@@ -1,35 +1,35 @@
-import sys, os
+import sys
+from os import environ, getcwd
+from os.path import abspath, realpath, dirname, commonprefix, exists
 
 
 def init(targets):
-    if not os.environ.get('REDO'):
+    if not environ.get('REDO'):
         # toplevel call to redo
         if not targets:
             targets.append('all')
         exename = sys.argv[0]
-        exenames = [os.path.abspath(exename),
-                    os.path.realpath(exename)]
-        dirnames = [os.path.dirname(p) for p in exenames]
-        trynames = ([os.path.abspath(p+'/../lib/redo') for p in dirnames] +
+        dirnames = map(dirname, (abspath(exename), realpath(exename)))
+        trynames = ([abspath(p+'/../lib/redo') for p in dirnames] +
                     [p+'/redo-sh' for p in dirnames] +
                     dirnames)
-
         dirs = sorted(set(trynames), key=trynames.index)
-        dirs.append(os.environ['PATH'])
-        os.environ['PATH'] = ':'.join(dirs)
-        os.environ['REDO'] = os.path.abspath(exename)
+        dirs.append(environ['PATH'])
+        environ['PATH'] = ':'.join(dirs)
+        environ['REDO'] = abspath(exename)
 
-    if not os.environ.get('REDO_BASE'):
-        base = os.path.commonprefix([os.path.abspath(os.path.dirname(t))
-                                     for t in targets] + [os.getcwd()])
+    if not environ.get('REDO_BASE'):
+        cwd = getcwd()
+        base = commonprefix([abspath(dirname(t)) for t in targets] +
+                            [cwd])
         bsplit = base.split('/')
         for i in range(len(bsplit)):
             newbase = '/'.join(bsplit[:-i])
-            if os.path.exists(newbase + '/.redo'):
+            if exists(newbase + '/.redo'):
                 base = newbase
                 break
-        os.environ['REDO_BASE'] = base
-        os.environ['REDO_STARTDIR'] = os.getcwd()
+        environ['REDO_BASE'] = base
+        environ['REDO_STARTDIR'] = cwd
 
         import state
         state.init()
