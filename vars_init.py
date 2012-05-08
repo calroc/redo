28cc7175be2923dc6590093fbbc18afeab9af6f1
Simon Forman <forman.simon@gmail.com>
2012-05-07 23:01:09 -0700

    Minor pythonification.

--- vars_init.py
+++ vars_init.py
@@ -1,31 +1,30 @@
 import sys, os
 
+
 def init(targets):
     if not os.environ.get('REDO'):
         # toplevel call to redo
-        if len(targets) == 0:
+        if not targets:
             targets.append('all')
-        exenames = [os.path.abspath(sys.argv[0]),
-                    os.path.realpath(sys.argv[0])]
+        exename = sys.argv[0]
+        exenames = [os.path.abspath(exename),
+                    os.path.realpath(exename)]
         dirnames = [os.path.dirname(p) for p in exenames]
         trynames = ([os.path.abspath(p+'/../lib/redo') for p in dirnames] +
                     [p+'/redo-sh' for p in dirnames] +
                     dirnames)
-        seen = {}
-        dirs = []
-        for k in trynames:
-            if not seen.get(k):
-                seen[k] = 1
-                dirs.append(k)
-        os.environ['PATH'] = ':'.join(dirs) + ':' + os.environ['PATH']
-        os.environ['REDO'] = os.path.abspath(sys.argv[0])
+
+        dirs = sorted(set(trynames), key=trynames.index)
+        dirs.append(os.environ['PATH'])
+        os.environ['PATH'] = ':'.join(dirs)
+        os.environ['REDO'] = os.path.abspath(exename)
 
     if not os.environ.get('REDO_BASE'):
         base = os.path.commonprefix([os.path.abspath(os.path.dirname(t))
                                      for t in targets] + [os.getcwd()])
         bsplit = base.split('/')
-        for i in range(len(bsplit)-1, 0, -1):
-            newbase = '/'.join(bsplit[:i])
+        for i in range(len(bsplit)):
+            newbase = '/'.join(bsplit[:-i])
             if os.path.exists(newbase + '/.redo'):
                 base = newbase
                 break
