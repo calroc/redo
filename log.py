7179bf634adc64809635e4d9eabc139e2e935be4
Simon Forman <forman.simon@gmail.com>
2012-05-10 20:36:45 -0700

    Use python logging library.
    Now we can send Redo status messages to all kinds of logging handlers
    with a few lines of code.

--- log.py
+++ log.py
@@ -1,49 +1,71 @@
-import sys, os
-import vars
+import sys, os, logging
+import vars as vars_
 
-# By default, no output colouring.
-RED    = ""
-GREEN  = ""
-YELLOW = ""
-BOLD   = ""
-PLAIN  = ""
 
+class RedoFormatter(logging.Formatter):
+
+    def format(self, record):
+        message = logging.Formatter.format(self, record)
+        message = self._colorize(record.levelno, message)
+        if vars_.DEBUG_PIDS:
+            message = '%d ' % os.getpid() + message
+        return message
+
+    def _colorize(self, levelno, message):
+        # Or rather don't colorize.
+        return ''.join(["redo  ", vars_.DEPTH, message])
+
+
+class ColorTermRedoFormatter(RedoFormatter):
+
+    colors = {
+        logging.INFO: "\x1b[32m", # Green.
+        logging.ERROR: "\x1b[31m", # Red.
+        logging.WARN: "\x1b[33m", # Yellow.
+        }
+    BOLD = "\x1b[1m"
+    PLAIN = "\x1b[m"
+
+    def _colorize(self, levelno, message):
+        color = self.colors.get(levelno, '')
+        return ''.join([color, "redo  ", vars_.DEPTH,
+                        self.BOLD, message, self.PLAIN])
+
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+_handler = logging.StreamHandler()
+_handler.setLevel(logging.DEBUG)
 if sys.stderr.isatty() and (os.environ.get('TERM') or 'dumb') != 'dumb':
-    # ...use ANSI formatting codes.
-    RED    = "\x1b[31m"
-    GREEN  = "\x1b[32m"
-    YELLOW = "\x1b[33m"
-    BOLD   = "\x1b[1m"
-    PLAIN  = "\x1b[m"
+    _formatter = ColorTermRedoFormatter()
+else:
+    _formatter = RedoFormatter()
+_handler.setFormatter(_formatter)
+logger.addHandler(_handler)
 
 
-def log_(s):
-    sys.stdout.flush()
-    if vars.DEBUG_PIDS:
-        sys.stderr.write('%d %s' % (os.getpid(), s))
-    else:
-        sys.stderr.write(s)
-    sys.stderr.flush()
+def log(s, *args):
+    logger.info(s.rstrip(), *args)
+
+def log_(s, *args):
+    logger.info(s.rstrip(), *args)
+
+def err(s, *args):
+    logger.error(s.rstrip(), *args)
+
+def warn(s, *args):
+    logger.warning(s.rstrip(), *args)
 
 
-def log(s):
-    log_(''.join([GREEN,  "redo  ", vars.DEPTH, BOLD, s, PLAIN]))
+# TODO: Use custom logging levels.
+def debug(s, *args):
+    if vars_.DEBUG >= 1:
+        logger.debug('-1- ' + s.rstrip(), *args)
 
-def err(s):
-    log_(''.join([RED,    "redo  ", vars.DEPTH, BOLD, s, PLAIN]))
+def debug2(s, *args):
+    if vars_.DEBUG >= 2:
+        logger.debug('-2- ' + s.rstrip(), *args)
 
-def warn(s):
-    log_(''.join([YELLOW, "redo  ", vars.DEPTH, BOLD, s, PLAIN]))
-
-
-def debug(s):
-    if vars.DEBUG >= 1:
-        log_('redo: %s%s' % (vars.DEPTH, s))
-def debug2(s):
-    if vars.DEBUG >= 2:
-        log_('redo: %s%s' % (vars.DEPTH, s))
-def debug3(s):
-    if vars.DEBUG >= 3:
-        log_('redo: %s%s' % (vars.DEPTH, s))
-
-
+def debug3(s, *args):
+    if vars_.DEBUG >= 3:
+        logger.debug('-3- ' + s.rstrip(), *args)
